a2da867f102fe2e8fde47127aa93ce17553dab02
premmm123 <poomsakppk@gmail.com>
2022-04-14 13:22:40 +0700

    add front/back login/regis

---
+++ backend/Routes/userRoute.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import User from '../Models/userModel.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const userRouter = express.Router();
+
+const generatePassword = async (password) => {
+    const passwordHashed = await bcrypt.hashSync(password, 10);
+    return passwordHashed;
+}
+
+userRouter.post('/register', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    try {
+        //check is this email have in DB
+        const userCheck = await User.findOne({ email: user.email });
+        if (userCheck) {
+            return res.json({ message: "THIS EMAIL ALREADY EXIST" });
+        }
+        //encrypt password
+        user.password = await generatePassword(user.password);
+        const create = await user.save();
+        create ? res.json({ message: "OK" }) : res.json({ message: "CREATE FAIL" });
+    } catch (err) {
+        res.json({ message: err });
+    }
+
+});
+
+userRouter.post('/login', async (req, res) => {
+    const user = await User.findOne({
+        email: req.body.email,
+    });
+    if (user) {
+        const checkPassword = bcrypt.compareSync(req.body.password, user.password);
+        if (checkPassword) {
+            res.json({
+                message: "OK",
+                _id: user.id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: jwt.sign({ name: user.name, email: user.email, id: user._id }, "Secret", { expiresIn: "2d" })
+            });
+        }
+        else {
+            return res.status(401).json({ message: 'Please check your username and password' });
+        }
+
+    } else {
+        res.status(401).json({ message: 'Please check your username and password' });
+    }
+});
+
+export default userRouter;
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import dotenv from 'dotenv';
+import userRouter from "./Routes/userRoute.js";
+import mongoose from "mongoose";
+import cors from 'cors';
+dotenv.config();
+
+const app = express();
+
+//connect DB
+mongoose.connect(process.env.MONGODB_URL).then(() => console.log("DB CONNECTED")).catch((err) => console.log("CAN'T CONNECT : ", err))
+
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors())
+
+//route
+app.get('/', (req, res) => res.send("DB CONNECTED"))
+app.get('/hello', (req, res) => {
+    res.send("hello world");
+})
+app.use('/api/users', userRouter);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("Sever is running on port ", port);
+})
