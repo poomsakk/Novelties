81bd1018736fcfba86f6989cc0b4ad0149257a4f
Surapat Wongprapaipak <69204357+surapat12@users.noreply.github.com>
2022-04-21 18:51:30 +0700

    six

--- frontend/src/Screen/PopularScreen.js
+++ frontend/src/Screen/PopularScreen.js
@@ -1,6 +1,7 @@
-import { Button } from 'react-bootstrap';
+import { Button,Form } from 'react-bootstrap';
 import data from "../sampleData.js";
 import React from 'react';
+
 
 export default function PopularScreen() {
     const user = data.Novel[1]
@@ -15,13 +16,26 @@ export default function PopularScreen() {
             <p>views = {user.allViewers}</p>
             <p>chapters = {novel}</p>
             <p>{data.Novel[1].image}</p>
-            <div>
+            <Form>
+                <Row>
+                    <Col xs={7}>
+                    <Form.Control placeholder="City" />
+                    </Col>
+                    <Col>
+                    <Form.Control placeholder="State" />
+                    </Col>
+                    <Col>
+                    <Form.Control placeholder="Zip" />
+                    </Col>
+                </Row>
+            </Form>
+        </>
+    )
+};
+
+<div>
                 <img src = {data.Novel[1].image} height={200} width={200}/>
                 <p>views = {user.allViewers}</p>
                 <p>chapters = {novel}</p>
                 <p>{data.Novel[1].image}</p>
             </div>
-        </>
-    )
-};
-
