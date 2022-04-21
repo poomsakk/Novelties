b112f7c235052f7444ff2e21506d475f3e358531
Surapat Wongprapaipak <69204357+surapat12@users.noreply.github.com>
2022-04-21 19:46:44 +0700

    tummaitaek

--- frontend/src/Screen/PopularScreen.js
+++ frontend/src/Screen/PopularScreen.js
@@ -21,6 +21,9 @@ export default function PopularScreen() {
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.(Novel Detail)
                     </Card.Text>
+                    <Card.Text>
+                        fdsfdsfsdfsdfsdfdsfdssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                    </Card.Text>
                     <Button variant="primary">อ่านนิยาย</Button>
                 </Card.Body>
             </Card>
