59e87ef4be8a04dc9aab8cf611772b2c3bce7c58
premmm123 <poomsakppk@gmail.com>
2022-04-21 23:09:19 +0700

    s

--- frontend/src/Screen/FavoriteScreen.js
+++ frontend/src/Screen/FavoriteScreen.js
@@ -1,15 +1,16 @@
 import { Button } from 'react-bootstrap';
 import data from "../sampleData.js";
 export default function FavoriteScreen() {
-    const user = data.Users[1]
-    const handle = (e) => {
-        console.log(user)
-    }
+    const user = data.Users[0]
+
     return (
         <>
             <h1>Favorite</h1>
-            <Button variant="danger" onClick={handle}>del</Button>
-            <p>coin have = {user.coin}</p>
+            {
+                user.favorite?.map((chap) => {
+                    return <h2>{chap.name}</h2>
+                })
+            }
         </>
     )
 };
