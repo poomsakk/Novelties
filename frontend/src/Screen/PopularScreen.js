5f487bf9892f35ae8465610e02c0221835a68eb9
Surapat Wongprapaipak <69204357+surapat12@users.noreply.github.com>
2022-04-21 19:37:55 +0700

    add card

--- frontend/src/Screen/PopularScreen.js
+++ frontend/src/Screen/PopularScreen.js
@@ -1,4 +1,4 @@
-import { Button,Form,Col,Row } from 'react-bootstrap';
+import { Button,Card } from 'react-bootstrap';
 import data from "../sampleData.js";
 import React from 'react';
 
@@ -13,35 +13,20 @@ export default function PopularScreen() {
         <>
             <h1>Popular</h1>
             {/* <Button variant="danger" onClick={handle}>del</Button> */}
-            <Form>
-                <Row>
-                    <Col>
-                        <img src = {data.Novel[0].image} height={200} width={200}/>
-                    </Col>
-                    <Col>
-                        <Row>
-                            <p>
-                                {' '}
-                                name = {data.Novel[0].name}
-
-
-                                {' '}
-                            </p>
-                        </Row>
-                        <Row>
-                            <p>views = {user.allViewers}</p>
-                        </Row>
-                    </Col>
-                    <Col>
-                    <p>chapters = {novel}</p>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                    
-                    </Col>
-                </Row>
-            </Form>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={data.Novel[0].image} height={200} width={200} />
+                <Card.Body>
+                    <Card.Title>{data.Novel[0].name}</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.(Novel Detail)
+                    </Card.Text>
+                    <Card.Text>
+                        Viewers : {data.Novel[0].allViewers}    Rating : {data.Novel[0].rating}    
+                    </Card.Text>
+                    <Button variant="primary">อ่านนิยาย</Button>
+                </Card.Body>
+            </Card>
         </>
     )
 };
