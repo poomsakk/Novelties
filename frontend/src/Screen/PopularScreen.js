24fd8ec2fe29f4fca1871c8f55eb82adaec7b401
Surapat Wongprapaipak <69204357+surapat12@users.noreply.github.com>
2022-04-21 17:24:53 +0700

    test

--- frontend/src/Screen/PopularScreen.js
+++ frontend/src/Screen/PopularScreen.js
@@ -1,7 +1,19 @@
-;plplplp
+import { Button } from 'react-bootstrap';
+import data from "../sampleData.js";
+
+export default function PopularScreen() {
+    const user = data.Users[1]
 
 
-
-
-
+    const handle = (e) => {
+        console.log(user)
+    }
+    return (
+        <>
+            <h1>Favorite</h1>
+            <Button variant="danger" onClick={handle}>del</Button>
+            <p>coin have = {user.coin}</p>
+        </>
+    )
+};
 
