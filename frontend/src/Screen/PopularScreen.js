268375bf2f69bf58e36961c0131066dd8ff9ffc7
Surapat Wongprapaipak <69204357+surapat12@users.noreply.github.com>
2022-04-21 18:44:47 +0700

    popular

--- frontend/src/Screen/PopularScreen.js
+++ frontend/src/Screen/PopularScreen.js
@@ -4,8 +4,7 @@ import React from 'react';
 
 export default function PopularScreen() {
     const user = data.Novel[1]
-    const novel = data.Novel.length
-
+    const novel = data.Novel[1].allChapter.length
     const handle = (e) => {
         console.log(user)
     }
@@ -13,11 +12,14 @@ export default function PopularScreen() {
         <>
             <h1>Popular</h1>
             <Button variant="danger" onClick={handle}>del</Button>
-            <p>coin have = {user.allViewers}</p>
-            <p>novel = {novel}</p>
+            <p>views = {user.allViewers}</p>
+            <p>chapters = {novel}</p>
             <p>{data.Novel[1].image}</p>
             <div>
                 <img src = {data.Novel[1].image} height={200} width={200}/>
+                <p>views = {user.allViewers}</p>
+                <p>chapters = {novel}</p>
+                <p>{data.Novel[1].image}</p>
             </div>
         </>
     )
