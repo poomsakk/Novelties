6d569baa522166d54eea2414773823044cfa8b7b
Surapat Wongprapaipak <69204357+surapat12@users.noreply.github.com>
2022-04-21 17:50:42 +0700

    tast loop

--- frontend/src/Screen/PopularScreen.js
+++ frontend/src/Screen/PopularScreen.js
@@ -2,18 +2,22 @@ import { Button } from 'react-bootstrap';
 import data from "../sampleData.js";
 
 export default function PopularScreen() {
-    const user = data.Novel[1]
+    
+    for (let i = 1; i < 10; i++) {
+        
+            const user = data.Novel[i]
 
 
-    const handle = (e) => {
-        console.log(user)
-    }
-    return (
-        <>
-            <h1>Popular</h1>
-            <Button variant="danger" onClick={handle}>del</Button>
-            <p>coin have = {user.allViewers}</p>
-        </>
+        const handle = (e) => {
+            console.log(user)
+        }
+        return (
+            <>
+                <h1>Popular</h1>
+                <Button variant="danger" onClick={handle}>del</Button>
+                <p>coin have = {user.allViewers}</p>
+            </>
     )
+    }
 };
 
