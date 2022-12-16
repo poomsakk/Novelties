feb94e97d50c295a21dc08c1792091da377b4078
premmm123 <poomsakppk@gmail.com>
2022-12-16 16:48:37 +0700

    change backend api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://api.novelties.poomsakk.com/'
+    baseURL: 'https://noveltiesbackend.poomsakk.com/'
 });
 
 // 'http://localhost:5000'
