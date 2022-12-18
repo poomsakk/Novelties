d2c32ad2020b402d740fae034311bac500aef61d
premmm123 <poomsakppk@gmail.com>
2022-12-18 22:16:36 +0700

    fix some api link

--- frontend/src/Screen/LoginSceen.js
+++ frontend/src/Screen/LoginSceen.js
@@ -13,7 +13,7 @@ export default function LoginScreen() {
 
     async function handleSubmit(e) {
         e.preventDefault();
-        const response = await fetch('http://localhost:5000/api/users/login', {
+        const response = await fetch('https://noveltiesbackend.poomsakk.com/api/users/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
--- frontend/src/Screen/WriterLoginScreen.js
+++ frontend/src/Screen/WriterLoginScreen.js
@@ -13,7 +13,7 @@ export default function WriterLoginScreen() {
 
     async function handleSubmit(e) {
         e.preventDefault();
-        const response = await fetch('http://localhost:5000/api/writer/login', {
+        const response = await fetch('https://noveltiesbackend.poomsakk.com/api/writer/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
